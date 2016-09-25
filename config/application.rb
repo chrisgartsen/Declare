b59f328a66d560e87710ac5673cb6558869f0486
Chris Gartsen <github@gartsen.nl>
2016-09-25 20:50:46 +0200

    Read content of local env file

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,19 @@ Bundler.require(*Rails.groups)
 
 module RailsStarterApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    # read a config file at location /config/local_env.yml
+    # this contains env variables
+    # DATABASE_USERNAME
+    # DATABASE_USERPASSWORD
+    #
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end
