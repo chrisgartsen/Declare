c05534253943d5d4ae337179a727e27862d6a3ff
Chris Gartsen <github@gartsen.nl>
2016-11-11 15:39:21 +0100

    Create user method stores the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
--- spec/controllers/users/create_spec.rb
+++ spec/controllers/users/create_spec.rb
@@ -6,11 +6,29 @@ RSpec.describe UsersController, type: :controller do
 
     context 'with valid input' do
 
-      it 'redirects to the home page'
+      it 'redirects to the home page' do
+        post :create, params: {user: FactoryGirl.attributes_for(:user)}
+        expect(response).to redirect_to(root_path)
+      end
 
-      it 'logs in as the created user'
+      it 'logs in as the created user' do
+        post :create, params: {user: FactoryGirl.attributes_for(:user)}
+        expect(session[:user_id]).to eq(User.last.id)
+      end
 
-      it 'creates and stores the user in the database'
+      it 'creates and stores an object in the database' do
+        expect {
+          post :create, params: {user: FactoryGirl.attributes_for(:user)}
+        }.to change(User, :count).by(1)
+      end
+
+      it 'creates the user with the passed parameters' do
+        post :create, params: {user: FactoryGirl.attributes_for(:user, name: 'PARAM NAME')}
+        user_new = FactoryGirl.build(:user, name: 'PARAM NAME')
+        user_created = User.last
+        expect(user_created.name).to eq(user_new.name)
+        expect(user_created.email).to eq(user_new.email)
+      end
 
     end
 
