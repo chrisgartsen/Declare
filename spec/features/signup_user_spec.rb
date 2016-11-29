27c26c768e0801c7f1460b468028d6183c798786
Chris Gartsen <github@gartsen.nl>
2016-11-29 21:42:56 +0100

    Complete signup feature spec

--- spec/features/signup_user_spec.rb
+++ spec/features/signup_user_spec.rb
@@ -11,6 +11,18 @@ RSpec.feature 'Signup user', type: :feature, js: true do
 
       expect(page.find('.pageheader')).to have_content('Sign up')
 
+      user = FactoryGirl.build(:user)
+
+      within('form') do
+        fill_in 'user_name', with: user.name
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        fill_in 'user_password_confirmation', with: user.password_confirmation
+        click_button 'Sign up'
+      end
+
+      expect(page.find('.navbar-text')).to have_content(user.name)
+
     end
 
   end
