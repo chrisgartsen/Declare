d59e01da1178a73e878e1f440e21060b022f8c3e
Chris Gartsen <github@gartsen.nl>
2016-12-14 21:10:57 +0100

    Add logout controller action

---
+++ spec/routing/sessions_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'SessionsRouting', type: :routing do
+
+  it 'routes to the login page' do
+    expect(get('/login')).to route_to('sessions#new')
+    expect(get: login_path).to route_to('sessions#new')
+  end
+
+  it 'routes to logout' do
+    expect(get('/logout')).to route_to('sessions#destroy')
+    expect(get: logout_path).to route_to('sessions#destroy')
+  end
+
+end
