0b50111cc161e9dba25ec3ec14cc0f110c6359d0
Chris Gartsen <github@gartsen.nl>
2017-01-05 20:38:34 +0100

    Show users in table on index page

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -46,6 +46,36 @@ RSpec.describe 'users/index', type: :view do
 
   describe '#table content' do
 
+    before(:each) do
+      @first_user = FactoryGirl.create(:user)
+      @second_user = FactoryGirl.create(:user, :additional_user)
+      assign(:users, [@first_user, @second_user])
+      render
+    end
+
+    it 'shows the correct number of users' do
+      expect(rendered).to have_selector('tbody tr', count: 2)
+    end
+
+    it 'shows the name' do
+      expect(rendered).to have_selector('tbody tr:first-child td#user-name', text: @first_user.name)
+      expect(rendered).to have_selector('tbody tr:last-child td#user-name', text: @second_user.name)
+    end
+
+    it 'shows the email' do
+      expect(rendered).to have_selector('tbody tr:first-child td#user-admin', text: @first_user.admin)
+      expect(rendered).to have_selector('tbody tr:last-child td#user-admin', text: @second_user.admin)
+    end
+
+    it 'shows the created_at' do
+      expect(rendered).to have_selector('tbody tr:first-child td#user-created-at', text: @first_user.created_at)
+      expect(rendered).to have_selector('tbody tr:last-child td#user-created-at', text: @second_user.created_at)
+    end
+
+    it 'shows the updated_at' do
+      expect(rendered).to have_selector('tbody tr:first-child td#user-updated-at', text: @first_user.updated_at)
+      expect(rendered).to have_selector('tbody tr:last-child td#user-updated-at', text: @second_user.updated_at)
+    end
 
   end
 
