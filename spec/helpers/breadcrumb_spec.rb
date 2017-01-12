2a6e1c7f173b2edce5eac2fd07262aad91811165
Chris Gartsen <github@gartsen.nl>
2017-01-12 09:20:41 +0100

    Breadcrumb helper works with multiple items

--- spec/helpers/breadcrumb_spec.rb
+++ spec/helpers/breadcrumb_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe BreadcrumbHelper, type: :helper do
 
   context 'with one active breadcrumb item passed' do
 
-    let(:output) { helper.breadcrumb('Test' => root_path) }
+    let(:output) { helper.breadcrumb('First' => root_path) }
 
     it 'renders the home location as an active item' do
       expect(output).to have_link('Home', href: root_path)
@@ -53,7 +53,57 @@ RSpec.describe BreadcrumbHelper, type: :helper do
     end
 
     it 'renders the passed breadcrumb as an active item' do
-      expect(output).to have_link('Test', href: root_path)
+      expect(output).to have_link('First', href: root_path)
+    end
+
+  end
+
+  context 'with two active breadcrumbs passed' do
+
+    let(:output) { helper.breadcrumb('First' => root_path, 'Second' => root_path) }
+
+    it 'renders the home location as an active item' do
+      expect(output).to have_link('Home', href: root_path)
+    end
+
+    it 'has three breadcrumb items' do
+      expect(output).to have_selector('li', count: 3)
+    end
+
+    it 'renders the passed breadcrumb as an active item' do
+      expect(output).to have_link('First', href: root_path)
+      expect(output).to have_link('Second', href: root_path)
+    end
+
+    it 'remebers the sequence order of items' do
+      expect(output).to have_selector('li:first-child', text: 'Home')
+      expect(output).to have_selector('li:nth-child(2)', text: 'First')
+      expect(output).to have_selector('li:last-child', text: 'Second')
+    end
+
+  end
+
+  context 'with one active and one static item passed' do
+
+    let(:output) { helper.breadcrumb('First' => root_path, 'Second' => '#') }
+
+    it 'renders the home location as an active item' do
+      expect(output).to have_link('Home', href: root_path)
+    end
+
+    it 'has three breadcrumb items' do
+      expect(output).to have_selector('li', count: 3)
+    end
+
+    it 'renders the passed breadcrumb as an active and static item' do
+      expect(output).to have_link('First', href: root_path)
+      expect(output).to have_selector('li.active', text: 'Second')
+    end
+
+    it 'remebers the sequence order of items' do
+      expect(output).to have_selector('li:first-child', text: 'Home')
+      expect(output).to have_selector('li:nth-child(2)', text: 'First')
+      expect(output).to have_selector('li:last-child', text: 'Second')
     end
 
   end
