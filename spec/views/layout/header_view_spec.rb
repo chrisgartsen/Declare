70d297dfe6319a809b78b3a45ae7cb64522f3c7c
Chris Gartsen <github@gartsen.nl>
2017-02-06 15:52:08 +0100

    Add project links to header

--- spec/views/layout/header_view_spec.rb
+++ spec/views/layout/header_view_spec.rb
@@ -41,6 +41,9 @@ RSpec.describe 'layouts/_header', type: :view do
       expect(rendered).not_to have_selector('a', text: "Log out")
     end
 
+    it 'does not have a projects link' do
+      expect(rendered).not_to have_selector('a', text: "Projects")
+    end
 
   end
 
@@ -66,6 +69,10 @@ RSpec.describe 'layouts/_header', type: :view do
 
     it 'does not have a view all users link' do
       expect(rendered).not_to have_link('Users', href: users_path)
+    end
+
+    it 'has a projects link' do
+      expect(rendered).to have_link('Projects', href: projects_path)
     end
 
   end
