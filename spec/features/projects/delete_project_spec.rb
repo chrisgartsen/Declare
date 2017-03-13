57d66732a4ec495841cf6f5322675c51c89ef632
Chris Gartsen <github@gartsen.nl>
2017-03-13 22:16:54 +0100

    Delete project feature

--- spec/features/projects/delete_project_spec.rb
+++ spec/features/projects/delete_project_spec.rb
@@ -2,13 +2,25 @@ RSpec.feature 'Delete project', type: :feature, js: true do
 
   context 'as a user' do
 
-    xit 'deletes a project' do
+    it 'deletes a project' do
       project = FactoryGirl.create(:project)
       login(project.user)
       navigate_to_page
       delete_project(project)
       check_outcome(project)
     end
+
+    private
+
+      def navigate_to_page
+        click_link 'list-projects'
+      end
+
+      def delete_project(project)
+        click_link "delete-project-#{project.id}"
+      end
+
+
 
   end
 
