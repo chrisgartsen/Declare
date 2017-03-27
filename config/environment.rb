b94cfa7c8fb31539477785522e27badfcf60f5e3
Chris Gartsen <github@gartsen.nl>
2017-03-27 17:41:30 +0200

    Set BootstrapFormbuilder as the default

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set bootstrap formbuilder as default
+ActionView::Base.default_form_builder = BootstrapFormbuilder
