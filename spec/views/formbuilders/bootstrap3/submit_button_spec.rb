158af9ab3b4e66ff5b5da20cd0fc2a91595e6fcf
Chris Gartsen <github@gartsen.nl>
2017-03-28 21:43:57 +0200

    Add specs to submit button group test

--- spec/views/formbuilders/bootstrap3/submit_button_spec.rb
+++ spec/views/formbuilders/bootstrap3/submit_button_spec.rb
@@ -2,7 +2,23 @@ RSpec.describe BootstrapFormbuilder do
 
   describe '#submit_button_group' do
 
+    let(:project) { FactoryGirl.build(:project) }
+    let(:builder) { BootstrapFormbuilder.new(:project, project, self, {})}
+    let(:output) { builder.submit_button_group() }
 
+    describe 'submit_button' do
+
+      it 'has a submit button with a default caption'
+
+      it 'accepts the caption as a parameter'
+
+      it 'has no back button by default'
+
+      it 'has a back button when a back-url is specified'
+
+      it 'is wrapped in a group div'
+
+    end
 
 
   end
