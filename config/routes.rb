c7641e27949da267c2bf28ed0f8832c4d7130194
Chris Gartsen <github@gartsen.nl>
2017-04-20 23:03:35 +0200

    Update about page route

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   # Static Pages
   get '/static_pages/:page', to: 'static_pages#show'
   get '/home',    to: 'static_pages#show', page: 'home'
-  get '/contact', to: 'static_pages#show', page: 'contact'
+  get '/contact', to: 'contact_messages#new'
   get '/about',   to: 'static_pages#show', page: 'about'
 
   root 'static_pages#show', page: 'home'
