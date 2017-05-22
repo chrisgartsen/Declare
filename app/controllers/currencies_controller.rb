f23e11276dd1d8aa457894752fc62fc53a794a60
Chris Gartsen <github@gartsen.nl>
2017-05-22 13:06:20 +0200

    Access to currencies controller restricted to admin

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,10 +1,17 @@
 class CurrenciesController < ApplicationController
 
+  before_action :check_authorisation
+
   include AuthenticationHelper
 
   def index
-    redirect_to login_path unless logged_in?
     @currencies = Currency.all
   end
+
+  private
+
+    def check_authorisation
+      redirect_to login_path unless is_admin?
+    end
 
 end
--- spec/controllers/currencies/index_spec.rb
+++ spec/controllers/currencies/index_spec.rb
@@ -2,20 +2,30 @@ RSpec.describe CurrenciesController, type: :controller do
 
   describe 'GET #index' do
 
-    context 'when logged in' do
+    context 'when logged in as an admin' do
 
       it 'renders the index view' do
-        set_authentication(FactoryGirl.create(:user))
+        set_authentication(FactoryGirl.create(:user, :admin))
         get :index
         expect(response).to render_template(:index)
       end
 
       it 'returns the currencies' do
-        set_authentication(FactoryGirl.create(:user))
+        set_authentication(FactoryGirl.create(:user, :admin))
         usd = FactoryGirl.create(:currency, :usd)
         eur = FactoryGirl.create(:currency, :euro)
         get :index
         expect(assigns(:currencies)).to match_array([usd, eur])
+      end
+
+    end
+
+    context 'when logged in as a user' do
+
+      it 'redirects to the login page' do
+        set_authentication(FactoryGirl.create(:user))
+        get :index
+        expect(response).to redirect_to(login_path)
       end
 
     end
