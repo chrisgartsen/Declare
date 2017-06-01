a7a0d25e7ea7bd448caad2a02fb0c2011fad401f
Chris Gartsen <github@gartsen.nl>
2017-06-01 21:34:59 +0200

    nav_tab helper accepts one link

--- app/helpers/nav_tab_helper.rb
+++ app/helpers/nav_tab_helper.rb
@@ -1,13 +1,38 @@
 module NavTabHelper
 
-  def nav_tab(items)
+  def nav_tab(items = {})
 
-    content_tag(:ul, class: 'nav nav-tabs') do
-      content_tag(:li, class: 'active') do
-        'Home'
-      end
+    return nil if items.empty?
+
+    return content_tag(:ul, class: 'nav nav-tabs') do
+      render_tabs(items)
     end
 
   end
 
+  private
+
+  def render_tabs(items)
+    output = ""
+    items.each do |caption, url |
+      output += render_tab(caption, url)
+    end
+    raw(output)
+  end
+
+  def render_tab(caption, url)
+    url == '#' ? render_current_tab(caption) : render_link_tab(caption, url)
+  end
+
+  def render_current_tab(caption)
+    content_tag(:li, class: 'active') do
+      link_to(caption, '#')
+    end
+  end
+
+  def render_link_tab(caption, url)
+    content_tag(:li) do
+      link_to(caption, url)
+    end
+  end
 end
--- spec/helpers/nav_tab_spec.rb
+++ spec/helpers/nav_tab_spec.rb
@@ -4,8 +4,20 @@ RSpec.describe NavTabHelper, type: :helper do
     expect(helper.nav_tab('Home' => '#')).to have_selector('ul.nav.nav-tabs')
   end
 
-  it 'requires at least one active location' do
-    expect(helper.nav_tab('Home' => '#')).to have_selector('li.active', text: 'Home')
+  it 'does not render anything without at least one location' do
+    expect(helper.nav_tab()).to eq nil
+
+  end
+
+  it 'accepts a current location' do
+    expect(helper.nav_tab('Home' => '#')).to have_selector('li.active')
+    expect(helper.nav_tab('Home' => '#')).to have_link('Home', href: '#')
+  end
+
+  it 'accepts a link location' do
+    expect(helper.nav_tab('Currencies' => currencies_path)).to have_selector('li')
+    expect(helper.nav_tab('Currencies' => currencies_path)).not_to have_selector('li.active')
+    expect(helper.nav_tab('Currencies' => currencies_path)).to have_link('Currencies', href: currencies_path)
   end
 
 end
