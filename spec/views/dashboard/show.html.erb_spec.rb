17fc34671f6690ca995bedd9fcfce1d0f2e758ba
Chris Gartsen <github@gartsen.nl>
2017-07-17 22:40:38 +0200

    Add username to dashboard page

--- spec/views/dashboard/show.html.erb_spec.rb
+++ spec/views/dashboard/show.html.erb_spec.rb
@@ -1,6 +1,26 @@
-RSpec.describe "dashboard/show.html.erb", type: :view do
+RSpec.describe "dashboards/show.html.erb", type: :view do
 
 
+  describe '#markup' do
 
+    let(:breadcrumb) {view.content_for(:breadcrumb) }
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @dashboard = Dashboard.new(user: @user)
+      set_authentication(@user)
+      assign(:dashboard, @dashboard)
+      render
+    end
+
+    it 'has a page header' do
+      expect(rendered).to have_selector('.pageheader', text: "Welcome #{current_user.name}")
+    end
+
+    it 'has a static reference to the index page' do
+      expect(breadcrumb).to have_selector('.breadcrumb li.active', text: 'Dashboard')
+    end
+
+  end
 
 end
