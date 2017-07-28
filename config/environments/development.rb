6b98c20a4ed07a56f6e45c82e706ea2d5ad88428
Chris Gartsen <github@gartsen.nl>
2017-07-28 20:03:09 +0200

    disble bullit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,12 +53,12 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.after_initialize do
-    Bullet.enable = true
-    Bullet.rails_logger = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.add_footer = true
+   # Bullet.enable = true
+   # Bullet.rails_logger = true
+   # Bullet.alert = true
+   # Bullet.bullet_logger = true
+   # Bullet.console = true
+   # Bullet.add_footer = true
   end
 
 end
