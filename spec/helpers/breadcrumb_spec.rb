552aaaff4adf0cf500666b97dc7af8657f77828c
Chris Gartsen <github@gartsen.nl>
2017-08-06 18:41:28 +0200

    Update breadcrumb helper to work with Bulma

--- spec/helpers/breadcrumb_spec.rb
+++ spec/helpers/breadcrumb_spec.rb
@@ -1,21 +1,17 @@
 RSpec.describe BreadcrumbHelper, type: :helper do
 
-  it 'is an ordered list' do
-    expect(helper.breadcrumb()).to have_selector('ol.breadcrumb')
+  it 'is an ordered list in a nav item' do
+    expect(helper.breadcrumb()).to have_selector('nav.breadcrumb ul')
   end
 
   context 'without any breadcrumb items passed' do
 
-    it 'renders the home location as a static breadcrumb endpoint' do
-      expect(helper.breadcrumb()).to have_selector('li.active', text: 'Home')
+    it 'renders the home location as the breadcrumb endpoint' do
+      expect(helper.breadcrumb()).to have_selector('li.is-active a', text: 'Home')
     end
 
     it 'has one breadcrumb item' do
       expect(helper.breadcrumb()).to have_selector('li', count: 1)
-    end
-
-    it 'has no active link breadcrumb items' do
-      expect(helper.breadcrumb()).not_to have_selector('a')
     end
 
   end
@@ -33,7 +29,7 @@ RSpec.describe BreadcrumbHelper, type: :helper do
     end
 
     it 'renders the passed breadcrumb as a static item' do
-      expect(output).to have_selector('li.active', text: 'Test')
+      expect(output).to have_selector('li.is-active a', text: 'Test')
     end
 
   end
@@ -95,13 +91,13 @@ RSpec.describe BreadcrumbHelper, type: :helper do
 
     it 'renders the passed breadcrumb as an active and static item' do
       expect(output).to have_link('First', href: root_path)
-      expect(output).to have_selector('li.active', text: 'Second')
+      expect(output).to have_selector('li.is-active a', text: 'Second')
     end
 
     it 'remebers the sequence order of items' do
-      expect(output).to have_selector('li:first-child', text: 'Home')
+      expect(output).to have_selector('li:first-child',  text: 'Home')
       expect(output).to have_selector('li:nth-child(2)', text: 'First')
-      expect(output).to have_selector('li:last-child', text: 'Second')
+      expect(output).to have_selector('li:last-child',   text: 'Second')
     end
 
   end
