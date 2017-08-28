0f16d39867fc9ae1208957b2427806e5018c0598
Chris Gartsen <github@gartsen.nl>
2017-08-28 22:12:22 +0200

    Update gemfile for Bulma-rails

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Set bootstrap formbuilder as default
+# Set Bulma formbuilder as default
 ActionView::Base.default_form_builder = BulmaFormbuilder
