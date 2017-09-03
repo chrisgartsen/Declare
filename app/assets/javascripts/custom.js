e56fafd1754fa59edb7ac62893678abed7495067
Chris Gartsen <github@gartsen.nl>
2017-09-03 17:18:34 +0200

    Clean up custom js file

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,26 +1,25 @@
-$(document).on('turbolinks:load', function() {
+$(document).on('turbolinks:load', function () {
 
   // Toggle show menu via navbar burger
 
-  $(".navbar-burger").click(function(){
+  $(".navbar-burger").click(function () {
     $(this).toggleClass('is-active');
     $("#" + $(this).data("target")).toggleClass('is-active');
   });
 
 
-  $('#log-in').click(function(event){
-
-   //event.preventDefault();
-   //$('#login-modal').addClass('is-active')
+  $('#log-in').click(function (event) {
+    event.preventDefault();
+    $('#login-modal').addClass('is-active');
 
   });
 
-  $('.modal-background').click(function(){
-    $('#login-modal').removeClass('is-active')
+  $('.modal-background').click(function () {
+    $('#login-modal').removeClass('is-active');
   });
 
-  $('#modal-close').click(function(){
-    $('#login-modal').removeClass('is-active')
+  $('#modal-close').click(function () {
+    $('#login-modal').removeClass('is-active');
   });
 
 });
